07692107234278c2ed7843de353037a23fcd06cc
Rakibul Hassan <59486222+rakibulll@users.noreply.github.com>
2021-04-04 10:38:22 -0400

    Update login.php

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ if (
 <html lang="en-US">
     <head>
         <title>Crud Applet</title>
-        <meta charset="hutf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- Latest compiled and minified Bootstrap CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
